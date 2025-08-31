972972fbebd52aec874e46b837b87ed35672d5ef
amirhosseinmirzaei9 <amir.h.mirzaei9@gmail.com>
2025-08-31 23:47:37 +0330

    deleted Google verification meta tag

--- keymancer_ui.py
+++ keymancer_ui.py
@@ -14,13 +14,6 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-st.markdown(
-    """
-    <meta name="google-site-verification" content="gukWY3PMRhLuWiRg7Q7cuX1tRZb__Ll7bDfqnwNwCbA" />    """,
-    unsafe_allow_html=True
-)
-
-
 
 st.title(":closed_lock_with_key: Keymancer")
 st.subheader("The ultimate password & security wizard 🧙‍♂️")
